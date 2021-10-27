145d20161b067735b2b2e9c86b8903feb851ffc1
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-10-28 01:03:49 +0200

    new files and first idea for the project

---
+++ student.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class Student {
+public:
+
+private:
+    std::string firstName_;
+    std::string lastName_;                                                                                                                                                                                                  
+
+};
